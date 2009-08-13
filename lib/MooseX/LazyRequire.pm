0804f0e04ff4fc87d4c1f90135ae52875597aafd
Florian Ragwitz <rafl@debian.org>
2009-08-13 02:15:26 +0200

    Add docs.

--- lib/MooseX/LazyRequire.pm
+++ lib/MooseX/LazyRequire.pm
@@ -1,8 +1,56 @@
 package MooseX::LazyRequire;
+# ABSTRACT: Required attributes which fail only when trying to use them
 
 use Moose::Exporter;
 use aliased 'MooseX::LazyRequire::Meta::Attribute::Trait::LazyRequire';
 use namespace::autoclean;
+
+=head1 SYNOPSIS
+
+    package Foo;
+
+    use Moose;
+    use MooseX::LazyRequire;
+
+    has foo => (
+        is           => 'ro',
+        lazy_require => 1,
+    );
+
+    has bar => (
+        is      => 'ro',
+        builder => '_build_bar',
+    );
+
+    sub _build_bar { shift->foo }
+
+
+    Foo->new(foo => 42); # succeeds, foo and bar will be 42
+    Foo->new(bar => 42); # succeeds, bar will be 42
+    Foo->new;            # fails, neither foo nor bare were given
+
+=head1 DESCRIPTION
+
+This module adds a C<lazy_require> option to Moose attribute declarations.
+
+The reader methods for all attributes with that option will throw an exception
+unless a value for the attributes was provided earlier by a constructor
+parameter or through a writer method.
+
+=head1 CAVEATS
+
+Apparently Moose roles don't have an attribute metaclass, so this module can't
+easily apply its magic to attributes defined in roles. If you want to use
+C<lazy_require> in role attributes, you'll have to apply the attribute trait
+yourself:
+
+    has foo => (
+        traits       => ['LazyRequire'],
+        is           => 'ro',
+        lazy_require => 1,
+    );
+
+=cut
 
 Moose::Exporter->setup_import_methods;
 
@@ -13,5 +61,11 @@ sub init_meta {
         attribute_metaclass_roles => [LazyRequire],
     );
 }
+
+=begin Pod::Coverage
+
+init_meta
+
+=end Pod::Coverage
 
 1;
